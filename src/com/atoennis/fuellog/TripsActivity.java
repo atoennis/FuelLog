216504014886e6cba244f6d0172c43b76bf55740
Adam <adamtoennis@gmail.com>
2013-08-18 19:16:31 -0400

    Initial creation of the Fuel Log project.

---
+++ src/com/atoennis/fuellog/TripsActivity.java
@@ -0,0 +1,22 @@
+package com.atoennis.fuellog;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TripsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_trips);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.trips, menu);
+		return true;
+	}
+
+}
