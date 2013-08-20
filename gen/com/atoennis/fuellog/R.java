24b6b3730c77d28454fcc431c6bfedf8bf0b5848
Adam <adamtoennis@gmail.com>
2013-08-20 19:23:48 -0400

    Changed theme to holo light.  Added add menu icon.

--- gen/com/atoennis/fuellog/R.java
+++ gen/com/atoennis/fuellog/R.java
@@ -26,6 +26,7 @@ public final class R {
     public static final class drawable {
         public static final int ic_drawer=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int ic_menu_new=0x7f020002;
     }
     public static final class id {
         public static final int action_settings=0x7f090003;
