14c9530e1a1393eb78cbb6861e4c3546106b885f
Adam <adamtoennis@gmail.com>
2013-09-24 21:15:45 -0400

    When editing, the form correctly populates with existing data now.

--- src/com/atoennis/fuellog/TripFormFragment.java
+++ src/com/atoennis/fuellog/TripFormFragment.java
@@ -85,7 +85,11 @@ public class TripFormFragment extends Fragment
             trip = (Trip) getArguments().getSerializable(EXTRA_TRIP);
         }
 
+        distanceInput = (EditText) view.findViewById(R.id.distance);
+        volumeInput = (EditText) view.findViewById(R.id.volume);
+        volumePriceInput = (EditText) view.findViewById(R.id.volume_price);
         datePicker = (Button) view.findViewById(R.id.date_picker);
+
         datePicker.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -99,12 +103,16 @@ public class TripFormFragment extends Fragment
         });
 
         Calendar cal = Calendar.getInstance();
+        if (trip != null)
+        {
+            cal.setTime(trip.date);
+
+            distanceInput.setText(Integer.toString(trip.distance));
+            volumeInput.setText(Double.toString(trip.volume));
+            volumePriceInput.setText(Double.toString(trip.volumePrice));
+        }
         setDateLabel(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
             cal.get(Calendar.DAY_OF_MONTH));
-
-        distanceInput = (EditText) view.findViewById(R.id.distance);
-        volumeInput = (EditText) view.findViewById(R.id.volume);
-        volumePriceInput = (EditText) view.findViewById(R.id.volume_price);
 
         return view;
     }
