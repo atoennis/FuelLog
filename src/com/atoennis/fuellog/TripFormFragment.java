0913212c8733b894d5c0b3eb1faa57c4ce3d7c16
Adam <adamtoennis@gmail.com>
2013-09-26 21:00:38 -0400

    fixed npe crash.

--- src/com/atoennis/fuellog/TripFormFragment.java
+++ src/com/atoennis/fuellog/TripFormFragment.java
@@ -108,9 +108,18 @@ public class TripFormFragment extends Fragment
         {
             cal.setTime(trip.date);
 
-            distanceInput.setText(Integer.toString(trip.distance));
-            volumeInput.setText(Double.toString(trip.volume));
-            volumePriceInput.setText(Double.toString(trip.volumePrice));
+            if (trip.distance != null)
+            {
+                distanceInput.setText(Integer.toString(trip.distance));
+            }
+            if (trip.volume != null)
+            {
+                volumeInput.setText(Double.toString(trip.volume));
+            }
+            if (trip.volumePrice != null)
+            {
+                volumePriceInput.setText(Double.toString(trip.volumePrice));
+            }
         }
         setDateLabel(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
             cal.get(Calendar.DAY_OF_MONTH));
